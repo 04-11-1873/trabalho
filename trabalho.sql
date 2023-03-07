1992c0ce540c0817826d6296e28f898a0e6d1da3
04-11-1873 <127133233+04-11-1873@users.noreply.github.com>
2023-03-07 18:46:36 -0300

    Add files via upload

---
+++ trabalho.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE trabalho;
+	CREATE TABLE user(
+		Nome varchar(15) NOT NULL;
+		Sobrenome varchar(15) NOT NULL;
+		Emaill varchar(100) NOT NULL;
+		Senha varchar(15) NOT NULL;
+		Numero varchar(11) NOT NULL;
+		Genero varchar(10) NOT NULL
+	);
+
+SELECT * FROM user;
